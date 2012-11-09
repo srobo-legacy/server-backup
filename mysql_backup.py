2a5a2aa7f272e854582893c7f0c02affea9a8a6a
Jeremy Morse <jmorse+git@studentrobotics.org>
2012-11-09 01:22:06 +0000

    Try to dump mysql databases to some output files.

--- mysql_backup.py
+++ mysql_backup.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python
 
 import ConfigParser
+import os
 
 config = ConfigParser.SafeConfigParser()
 config.read("backup.ini")
-list_of_dbs = config.get("mysql", "databases")
-print repr(list_of_dbs)
+list_of_dbs_str = config.get("mysql", "databases")
+
+# Turn ugly ugly ini string into a list of db names.
+list_of_dbs = list_of_dbs_str.split(',')
+for s in list_of_dbs:
+    s.strip()
+
+try:
+  os.makedirs('mysql_dbs', 0600)
+except os.error: 
+  pass
+
+for s in list_of_dbs:
+    os.system("mysqldump {0} -u root -p > mysql_dbs/{1}".format(s, s))
