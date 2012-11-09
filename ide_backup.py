556984faff9b66713f9d100564d6aed6ba2dc79b
Jeremy Morse <jmorse+git@studentrobotics.org>
2012-11-09 02:12:11 +0000

    Actually copy ide repos into the relevant directory.

--- ide_backup.py
+++ ide_backup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import glob, os, sys
+import glob, os, sys, shutil
 
 # le privacy
 os.umask(0177)
@@ -13,7 +13,11 @@ except os.error:
 # Back up user repos: we only want the _master_ copies of everything, not the
 # user checkouts of repos, which I understand are only used for staging changes
 # before being pushed back to master.
-list_of_dirs = glob.glob('/var/www/html/ide/repos/*/master')
+list_of_dirs = glob.glob('/var/www/html/ide-off/repos/*/master')
 for dir in list_of_dirs:
+    # Extract what the team number is
     teamnum = os.path.basename(os.path.dirname(dir))
-    print teamnum
+    targetdir = 'ide_repos/{0}'.format(teamnum)
+
+    # Copy the master dir into there, with all relevant git repos.
+    shutil.copytree(dir, targetdir)
