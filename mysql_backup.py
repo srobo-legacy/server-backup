a905c3fbeb6e6d91d942c0a63e4dfc87b76a91b8
Jeremy Morse <jmorse+git@studentrobotics.org>
2012-11-09 01:10:50 +0000

    Extract a list of databases from an ini config file

--- mysql_backup.py
+++ mysql_backup.py
@@ -1,1 +1,8 @@
+#!/usr/bin/python
+
 import ConfigParser
+
+config = ConfigParser.SafeConfigParser()
+config.read("backup.ini")
+list_of_dbs = config.get("mysql", "databases")
+print repr(list_of_dbs)
