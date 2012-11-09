9dcaa809eeff3e6c8af01546e054d63b57ef0a39
Jeremy Morse <jmorse+git@studentrobotics.org>
2012-11-09 18:18:51 +0000

    Start reaping secret keys.
    (The server certificate is, in fact, not secret, but the key is entirely
    pointless unless we have the CA's signature on it).

--- secrets_backup.py
+++ secrets_backup.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python
+
+import os
+import tarfile
+
+# Produce a tarfile containing our secret keys.
+
+output = tarfile.open('secret_keys_backup.tar.gz', mode='w:gz')
+
+def my_addfile(tarname, srcfile):
+    thestat = os.stat(srcfile)
+    info = tarfile.TarInfo(name=tarname)
+    info.size = thestat.st_size
+    output.addfile(tarinfo=info, fileobj=open(srcfile))
+
+my_addfile('www.studentrobotics.org.crt', '/etc/pki/tls/certs/www.studentrobotics.org.crt')
+
+output.close()
